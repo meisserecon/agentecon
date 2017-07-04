04cbaf22321faad6cde0291b4c1814b1d8992a66
David Fuersinger <df@taskfleet.com>
2017-07-04 18:23:27 +0200

    [Update] Tradeview: Add global node with local coordinate system and set link coordinates in local system

--- arena/WebContent/js/tradeview.js
+++ arena/WebContent/js/tradeview.js
@@ -3,8 +3,8 @@ class Tradeview {
     this.stage;
     this.NODE_RADIUS = 50;
     this.nodes = [
-      { x: '200', y: '300' },
-      { x: '700', y: '300' }
+      { x: '200', y: '500' },
+      { x: '700', y: '400' }
     ];
     this.links = [
       { source: [this.nodes[0].x,this.nodes[0].y], target: [this.nodes[1].x, this.nodes[1].y] },
@@ -12,7 +12,10 @@ class Tradeview {
       { source: [this.nodes[0].x,this.nodes[0].y], target: [this.nodes[1].x, this.nodes[1].y] },
       { source: [this.nodes[0].x,this.nodes[0].y], target: [this.nodes[1].x, this.nodes[1].y] },
       { source: [this.nodes[0].x,this.nodes[0].y], target: [this.nodes[1].x, this.nodes[1].y] },
-      { source: [this.nodes[0].x,this.nodes[0].y], target: [this.nodes[1].x, this.nodes[1].y] }
+      { source: [this.nodes[0].x,this.nodes[0].y], target: [this.nodes[1].x, this.nodes[1].y] },
+      // { source: [this.nodes[1].x,this.nodes[1].y], target: [this.nodes[0].x, this.nodes[0].y] },
+      // { source: [this.nodes[1].x,this.nodes[1].y], target: [this.nodes[0].x, this.nodes[0].y] },
+      // { source: [this.nodes[1].x,this.nodes[1].y], target: [this.nodes[0].x, this.nodes[0].y] }
     ];
 
     this.init();
@@ -72,66 +75,85 @@ class Tradeview {
 
     let _this = this;
 
-    let link = this.stage.selectAll('.link')
+    let oppositeLeg = this.nodes[1].y - this.nodes[0].y;
+    let adjacentLeg = this.nodes[1].x - this.nodes[0].x;
+    let alphaRad = Math.atan(oppositeLeg/adjacentLeg) * (180 / Math.PI);
+
+    let linksWrapper = this.stage.append('g')
+      .attr('class', 'links__wrapper')
+      .attr('transform', 'translate(' + this.nodes[0].x + ',' + this.nodes[0].y + ') rotate(' + alphaRad + ')');
+
+    linksWrapper.append('path')
+      .attr('d', 'M0 0 L500 0')
+      .attr('stroke', 'pink');
+
+    // coordinates for target node in local coordinate system
+    let xSource = 0;
+    let ySource = 0;
+    let xTarget = (this.nodes[1].x - this.nodes[0].x)/Math.cos(alphaRad * Math.PI / 180);
+    let yTarget = 0;
+
+    linksWrapper.append('circle')
+      .attr('cx', xTarget)
+      .attr('cy', yTarget)
+      .attr('r', 10)
+      .attr('fill', 'black');
+
+    let link = linksWrapper.selectAll('.link')
       .data(this.links)
       .enter()
       .append('path')
       .attr('class', 'link')
       .attr('d', function(d, i) {
-        let x0 = parseInt(d.source[0], 10) + _this.NODE_RADIUS * Math.cos(i * 0.3);
-        let y0 = parseInt(d.source[1], 10) - _this.NODE_RADIUS * Math.sin(i * 0.3);
-        let x1 = parseInt(d.target[0], 10) - _this.NODE_RADIUS * Math.cos(i * 0.3);
-        let y1 = parseInt(d.target[1], 10) - _this.NODE_RADIUS * Math.sin(i * 0.3);
+        let j = i + 2;
+        let x0 = xSource + _this.NODE_RADIUS * Math.cos((i + 1) * 0.3); // 0.3 rad ^= 17.2 deg
+        let y0 = ySource - _this.NODE_RADIUS * Math.sin((i + 1) * 0.3);
+        let x1 = xTarget - _this.NODE_RADIUS * Math.cos((i + 1) * 0.3);
+        let y1 = yTarget - _this.NODE_RADIUS * Math.sin((i + 1) * 0.3);
 
-        let cx0 = (2 + i * 0.1) * x0 - d.source[0];
-        let cx1 = (2 - i * 0.01) * x1 - d.target[0];
-        let cy0 = (2 - i * 0.1) * y0 - d.source[1];
-        let cy1 = (2 - i * 0.07) * y1 - d.target[1];
+        let cx0 = j * (x0);
+        let cx1 = j * (x1 - xTarget) + xTarget;
+        let cy0 = j * (y0);
+        let cy1 = j * (y1 - yTarget);
 
-        if (i === 0) {
-          cy0 = y0 - _this.NODE_RADIUS / 2;
-          cy1 = y1 - _this.NODE_RADIUS / 2;
-        }
+        // if (i === 0) {
+        //   cy0 = y0 - _this.NODE_RADIUS * 0.5;
+        //   cy1 = y1 - _this.NODE_RADIUS * 0.5;
+        // }
 
-        _this.stage
+        linksWrapper
           .append('circle')
           .attr('cx', x0)
           .attr('cy', y0)
           .attr('r', 5)
           .attr('fill', 'red');
 
-        _this.stage
+        linksWrapper
           .append('circle')
           .attr('cx', x1)
           .attr('cy', y1)
           .attr('r', 2)
           .attr('fill', 'red');
 
-        _this.stage
+        linksWrapper
           .append('circle')
           .attr('cx', cx0)
           .attr('cy', cy0)
           .attr('r', 5)
           .attr('fill', 'deepskyblue');
 
-        _this.stage
+        linksWrapper
           .append('circle')
           .attr('cx', cx1)
           .attr('cy', cy1)
-          .attr('r', 5)
+          .attr('r', 3)
           .attr('fill', 'deepskyblue');
 
-        let oppositeLeg = d.target[1] - d.source[1];
-        let adjacentLeg = d.target[0] - d.source[0];
-        let alpha = Math.atan(oppositeLeg/adjacentLeg) * (180 / Math.PI);
         return 'M' + x0 + ' ' + y0 + ' C ' + cx0 + ' ' + cy0 + ', ' + cx1 + ' ' + cy1 + ', ' + x1 + ' ' + y1;
       })
       .attr('marker-end', function(d) { return 'url(#marker)'; });
 
     this.stage.append('svg:defs')
-      .selectAll('marker')
-      .data([true])
-      .enter()
       .append('svg:marker')
         .attr('id', 'marker')
         .attr('class', 'marker')
