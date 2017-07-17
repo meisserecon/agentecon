5f3f6724db7f6b823a7e40349d4ed97ce50b166a
Roni Oeschger <ro@taskfleet.com>
2017-07-17 11:47:44 +0200

    [Update] Tradeview: Display controls only when simulation length could get fetched

--- frontend/src/components/Tradeview.vue
+++ frontend/src/components/Tradeview.vue
@@ -2,19 +2,15 @@
   <div>
     <h1>Tradeview</h1>
 
-    <div v-if="loading">
-      Loading...
-    </div>
-
-    <div v-if="tradeGraphData">
+    <div v-if="loaded">
       <button v-if="simDay > 0" @click="prevStep"><</button>
       <button v-if="simDay < simLength" @click="nextStep">></button>
       <button v-if="simDay < simLength" @click="playing = !playing">{{ playing ? '||' : '|>' }}</button>
       <button v-if="simDay > 0" @click="simDay = 0"><<</button>
       <input v-model.number="simDay">
-    </div>
 
-    <tradegraph :graphdata="tradeGraphData"></tradegraph>
+      <tradegraph :graphdata="tradeGraphData"></tradegraph>
+    </div>
   </div>
 </template>
 
@@ -31,7 +27,7 @@ export default {
     return {
       apiUrl: config.apiURL,
       tradeGraphData: null,
-      loading: false,
+      loaded: false,
       playing: false,
       playInterval: null,
       simId: this.$route.query.sim,
@@ -52,6 +48,7 @@ export default {
     .then(
       (info) => {
         this.simLength = info.days;
+        this.loaded = true;
       },
     )
     .catch(error => config.alertError(error));
@@ -97,19 +94,13 @@ export default {
     fetchData() {
       // fetchData has all needed state data in URL
       this.tradeGraphData = null;
-      this.loading = true;
 
       fetch(
         `${this.apiUrl}/tradegraph?sim=${this.simId}&day=${this.simDay}&agents=${this.simAgents}&step=${this.simStep}`,
         config.xhrConfig,
       )
       .then(config.handleFetchErrors)
-      .then(
-        (response) => {
-          this.loading = false;
-          return response.json();
-        },
-      )
+      .then(response => response.json())
       .then(
         (tradeGraphData) => {
           this.tradeGraphData = tradeGraphData;
